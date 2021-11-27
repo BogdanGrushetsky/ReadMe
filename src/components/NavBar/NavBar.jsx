c3735a59de79b34ee1e2afe69f6492a03aac88de
Shafran-Ivan <shafranvanya@gmail.com>
2021-11-27 16:29:45 +0200

    Header

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import style from "./NavBar.css"
+import logo from "./../../img/logo_with_name.svg"
+
+const Link = (props) => {
+  return <a className="NavBar__link" href={props.href}>{props.children}</a>
+}
+
+const NavBar = (props) => {
+  return (
+    <nav className="NavBar">
+      <div className="NavBar__container"> 
+        <Link href="/aboutUs">Про нас</Link>
+        <Link href="/settings">Налаштування</Link>
+        <Link href="/cabinet">Особистий кабінет</Link>
+        <div className="NavBar_Burger"/>
+        
+        <img src={logo} className="NavBar__logo"/>
+      </div>
+
+    </nav>
+  )
+}
+
+export default NavBar
