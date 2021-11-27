e351a740738a3d7eb048c300b32beb83f8427745
Karina Neskorozhena <88102302+knesko@users.noreply.github.com>
2021-11-27 19:20:02 +0200

    designed login form

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import { MenuItem } from './components/Menu/Menu';
 import Menu from './components/Menu/Menu';
 
 import "./components/main.css"
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <div>
       <NavBar/>
-
+        <Login/>
       <div className='main'>
-        <Menu>
-          <MenuItem src={people}/>
-          
-        </Menu>
+
+        {/*<Menu>*/}
+        {/*  <MenuItem src={people}/>*/}
+        {/*  */}
+        {/*</Menu>*/}
 
       </div>
 
